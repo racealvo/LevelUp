465e29c6bd88a0855a764224afffdf4311da6c8a
rsalvo <rsalvo@roamdata.com>
2017-02-05 23:58:48 -0500

    v2 - a little more elaborate - more inheritance.

--- LevelUp/Program.cs
+++ LevelUp/Program.cs
@@ -4,43 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LevelUp
+namespace LevelUpSandbox
 {
-    /// <summary>
-    /// This is the workhorse.  It contains the class counter.
-    /// Encapsulate counter - to be safe.  This will require instance access rather than class access
-    /// </summary>
-    /// <typeparam name="T">Generics: T - is the class type (to be counted)</typeparam>
-    public class Parent<T> 
+    public class Counter
     {
-        private static int counter;
+        public Type ClassType { get; set; }
+        public int Count { get; set; }
+    }
 
-        public Parent()
+    public class Parent
+    {
+        private static List<Counter> listCounters = new List<Counter>();
+        public static int TheCount(Type type)
         {
-            counter++;
+            Counter myClass = listCounters.Find(counterObject => counterObject.ClassType == type);
+            int count = (myClass == null) ? 0 : myClass.Count;
+            return count;
         }
 
-        public static int Counter { get { return counter; } }
+        //private static Counter FindCounter
+
+        public Parent(Type type)
+        {
+            Counter myClass = listCounters.Find(counterObject => counterObject.ClassType == type);
+            if (myClass == null)
+            {
+                myClass = new Counter();
+                myClass.ClassType = type;
+                myClass.Count++;
+                listCounters.Add(myClass);
+            }
+            else
+            {
+                myClass.Count++;
+            }
+        }
     }
 
-    public class CountMe
+    public class CountMe : Parent
     {
+        public CountMe() : base(typeof(CountMe))
+        {
+        }
     }
 
-    public class CountMeToo
+    public class CountMeToo : Parent
     {
+        public CountMeToo() : base(typeof(CountMeToo))
+        {
+        }
     }
 
-    public class AndMe
+    public class AndMe : Parent
     {
+        public AndMe() : base(typeof(AndMe))
+        {
+        }
     }
 
-    public class Counted
+    public class Counted : Parent
     {
+        public Counted() : base(typeof(Counted))
+        {
+        }
     }
 
-    public class ZeroInstances
+    public class ZeroInstances : Parent
     {
+        public ZeroInstances() : base(typeof(ZeroInstances))
+        {
+        }
     }
 
     class Program
@@ -52,30 +85,27 @@ namespace LevelUp
         /// <typeparam name="T">Generics: T - is the class type (to be counted)</typeparam>
         /// <param name="instances">number of class objects to add to the list</param>
         /// <returns>List of classes.</returns>
-        static List<Parent<T>> Factory<T>(int instances)
+        static List<T> Factory<T>(int instances)
         {
-            List<Parent<T>> classList = new List<Parent<T>>();
+            List<T> classList = new List<T>();
             for (int i = 0; i < instances; i++)
             {
-                classList.Add(new Parent<T>());
+                classList.Add((T)Activator.CreateInstance<T>());
             }
-            Console.WriteLine("{0} {1}", typeof(T).Name, Parent<T>.Counter);
+            Console.WriteLine("{0} {1}", typeof(T).Name, Parent.TheCount(typeof(T)));
 
             return classList;
         }
 
-        /// <summary>
-        /// This is a little contrived.  Since everything is being stored in lists, 
-        /// we could simply count the members in each list.
-        /// </summary>
-        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            List<Parent<CountMe>> countMe       = Factory<CountMe>(5);
-            List<Parent<CountMeToo>> countMeToo = Factory<CountMeToo>(4);
-            List<Parent<AndMe>> andMe           = Factory<AndMe>(12);
-            List<Parent<Counted>> counted       = Factory<Counted>(5);
-            List<Parent<ZeroInstances>> zeros   = Factory<ZeroInstances>(0);
+            List<CountMe> countMe = Factory<CountMe>(5);
+            List<CountMeToo> countMeToo = Factory<CountMeToo>(4);
+            List<AndMe> andMe = Factory<AndMe>(12);
+            List<Counted> counted = Factory<Counted>(5);
+            List<ZeroInstances> zeros = Factory<ZeroInstances>(0);
+            zeros = Factory<ZeroInstances>(-1);
         }
     }
 }
+
